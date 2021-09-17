fb06d11f68593acd1e9f6d0333dfb18caa9ee880
Youssef Abdelaziz <yabdelaziz.123@gmail.com>
2021-09-17 14:51:08 +0200

    updateCustomerTestController

--- src/test/java/com/jumia/demo/controller/CustomerControllerTest.java
+++ src/test/java/com/jumia/demo/controller/CustomerControllerTest.java
@@ -1,7 +1,6 @@
-package controller;
+package com.jumia.demo.controller;
 
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,27 +14,28 @@ import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.jumia.demo.controller.CustomerController;
 import com.jumia.demo.dto.CustomerDto;
 import com.jumia.demo.dto.PhoneNoRegex;
 import com.jumia.demo.repository.CustomerRepository;
-import com.jumia.demo.service.CustomerServiceImpl;
+import com.jumia.demo.service.CustomerService;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = CustomerController.class)
+@WebAppConfiguration
 public class CustomerControllerTest {
 
+    @Autowired
 	MockMvc mockMvc;
-
+    
     @Autowired
     protected WebApplicationContext wac;
     
@@ -46,13 +46,13 @@ public class CustomerControllerTest {
     CustomerController customerController;
 
     @MockBean
-    CustomerServiceImpl CustomerServiceImpl;
+    CustomerService customerService;
     
     private List<CustomerDto> customerDtos;
     
 	@Before
 	public void setUp() throws Exception{
-		this.mockMvc = MockMvcBuilders.standaloneSetup(new CustomerController(CustomerServiceImpl)).build();
+		this.mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
 		//mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
 		customerDtos = new ArrayList<CustomerDto>();
 		CustomerDto customer1 = new CustomerDto();
@@ -74,7 +74,7 @@ public class CustomerControllerTest {
     public void testGetCustomersByCountry() throws Exception {
 
         // Mocking service
-        when(new CustomerServiceImpl(customerRepository).getAllCustomers(PhoneNoRegex.MOROCCO)).thenReturn(customerDtos);
+        when(customerService.getAllCustomers(PhoneNoRegex.MOROCCO)).thenReturn(customerDtos);
 
         mockMvc.perform(get("/api/customers/?country=MOROCCO").contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
